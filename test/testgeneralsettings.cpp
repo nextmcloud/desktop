9d331a40346cd91174e61d36730b19c0b9eb0a42
Ankur Bavisa <Ankur.Bavisa@t-systems.com>
2023-06-23 16:17:51 +0530

    code rearranged

--- test/testgeneralsettings.cpp
+++ test/testgeneralsettings.cpp
@@ -53,12 +53,15 @@ private slots:
 
         GeneralSettings genSetting(new QWidget());
 
+        //Advanced
         QCOMPARE(genSetting._ui->newExternalStorage->isHidden(), true);
+        QCOMPARE(cfgFile.confirmExternalStorage(), true);
         QCOMPARE(genSetting._ui->showInExplorerNavigationPaneCheckBox->isHidden(), true);
+        QCOMPARE(cfgFile.showInExplorerNavigationPane(), true);
         QCOMPARE(genSetting._ui->ignoredFilesButton->isHidden(), true);
         QCOMPARE(genSetting._ui->debugArchiveButton->isHidden(), true);
-        QCOMPARE(cfgFile.showInExplorerNavigationPane(), true);
-        QCOMPARE(cfgFile.confirmExternalStorage(), true);
+
+
     }
 
     void testLoadMiscSettings_Checked()
@@ -71,8 +74,11 @@ private slots:
 
         QMetaObject::invokeMethod( &genSetting, "loadMiscSettings");
 
+        QCOMPARE(genSetting._ui->serverNotificationsCheckBox->isChecked(), true);
         QCOMPARE(genSetting._ui->showInExplorerNavigationPaneCheckBox->isChecked(), true);
         QCOMPARE(cfgFile.confirmExternalStorage(), true);
+
+
     }
 
     void testLoadMiscSettings_UnChecked()
@@ -85,108 +91,12 @@ private slots:
 
         QMetaObject::invokeMethod( &genSetting, "loadMiscSettings");
 
+        QCOMPARE(genSetting._ui->serverNotificationsCheckBox->isChecked(), false);
         QCOMPARE(genSetting._ui->showInExplorerNavigationPaneCheckBox->isChecked(), false);
         QCOMPARE(cfgFile.confirmExternalStorage(), true);
+
+
     }
-
-    /* UI based (event driven) test cases */
-    void test_serverNotificationsCheckBox_Checked()
-    {
-        ConfigFile cfgFile;
-        FolderMan folderMan(new QObject());
-        GeneralSettings genSetting(new QWidget());
-
-        /* Initial set un-checked */
-        genSetting._ui->serverNotificationsCheckBox->setChecked(false);
-        QVERIFY( genSetting._ui->serverNotificationsCheckBox->checkState() == Qt::Unchecked );
-
-        /*to track the SIGNAL emit or not */
-        QSignalSpy spy(genSetting._ui->serverNotificationsCheckBox, SIGNAL(toggled(bool)));
-
-        /* generate event/emit signal */
-        genSetting._ui->serverNotificationsCheckBox->toggle();
-
-        /* verify SIGNAL emit */
-        QCOMPARE(spy.count(), 1);
-
-        /* verify SLOT data */
-        QCOMPARE(cfgFile.optionalServerNotifications(), true);
-        QVERIFY( genSetting._ui->serverNotificationsCheckBox->checkState() == Qt::Checked );
-    }
-
-    void test_serverNotificationsCheckBox_UnChecked()
-    {
-        ConfigFile cfgFile;
-        FolderMan folderMan(new QObject());
-        GeneralSettings genSetting(new QWidget());
-
-        /* Initial set checked */
-        genSetting._ui->serverNotificationsCheckBox->setChecked(true);
-        QVERIFY( genSetting._ui->serverNotificationsCheckBox->checkState() == Qt::Checked );
-
-        /*to track the SIGNAL emit or not */
-        QSignalSpy spy(genSetting._ui->serverNotificationsCheckBox, SIGNAL(toggled(bool)));
-
-        /* generate event/emit signal */
-        genSetting._ui->serverNotificationsCheckBox->toggle();
-
-        /* verify SIGNAL emit */
-        QCOMPARE(spy.count(), 1);
-
-        /* verify SLOT data */
-        QCOMPARE(cfgFile.optionalServerNotifications(), false);
-        QVERIFY( genSetting._ui->serverNotificationsCheckBox->checkState() == Qt::Unchecked );
-    }
-
-#if defined(BUILD_UPDATER)
-    void test_autoCheckForUpdatesCheckBox_Checked()
-    {
-        ConfigFile cfgFile;
-        FolderMan folderMan(new QObject());
-        GeneralSettings genSetting(new QWidget());
-
-        /* Initial set un-checked */
-        genSetting._ui->autoCheckForUpdatesCheckBox->setChecked(false);
-        QVERIFY( genSetting._ui->autoCheckForUpdatesCheckBox->checkState() == Qt::Unchecked );
-
-        /*to track the SIGNAL emit or not */
-        QSignalSpy spy(genSetting._ui->autoCheckForUpdatesCheckBox, SIGNAL(toggled(bool)));
-
-        /* generate event/emit signal */
-        genSetting._ui->autoCheckForUpdatesCheckBox->toggle();
-
-        /* verify SIGNAL emit */
-        QCOMPARE(spy.count(), 1);
-
-        /* verify SLOT data */
-        QVERIFY( genSetting._ui->autoCheckForUpdatesCheckBox->checkState() == Qt::Checked );
-        QCOMPARE(cfgFile.autoUpdateCheck(), true);
-    }
-
-    void test_autoCheckForUpdatesCheckBox_UnChecked()
-    {
-        ConfigFile cfgFile;
-        FolderMan folderMan(new QObject());
-        GeneralSettings genSetting(new QWidget());
-
-        /* Initial set checked */
-        genSetting._ui->autoCheckForUpdatesCheckBox->setChecked(true);
-        QVERIFY( genSetting._ui->autoCheckForUpdatesCheckBox->checkState() == Qt::Checked );
-
-        /*to track the SIGNAL emit or not */
-        QSignalSpy spy(genSetting._ui->autoCheckForUpdatesCheckBox, SIGNAL(toggled(bool)));
-
-        /* generate event/emit signal */
-        genSetting._ui->autoCheckForUpdatesCheckBox->toggle();
-
-        /* verify SIGNAL emit */
-        QCOMPARE(spy.count(), 1);
-
-        /* verify SLOT data */
-        QCOMPARE(cfgFile.autoUpdateCheck(), false);
-        QVERIFY( genSetting._ui->autoCheckForUpdatesCheckBox->checkState() == Qt::Unchecked );
-    }
-#endif // defined(BUILD_UPDATER)
 
     void test_Screen_text()
     {
@@ -198,7 +108,6 @@ private slots:
 
         QCOMPARE(genSetting._ui->groupBox->title(), "Advanced");
         QCOMPARE(genSetting._ui->newFolderLimitCheckBox->text(), "Ask for confirmation before synchronizing folders larger than");
-        QCOMPARE(genSetting._ui->crashreporterCheckBox->text(), "S&how crash reporter");
         QCOMPARE(genSetting._ui->updatesGroupBox->title(), "Updates");
         QCOMPARE(genSetting._ui->autoCheckForUpdatesCheckBox->text(), "&Automatically check for Updates");
         QCOMPARE(genSetting._ui->restartButton->text(), "&Restart && Update");
