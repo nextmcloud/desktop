ceb808a90252d7252b141133cf8db11d277b2691
Eugen Fischer <feugen@MBP-von-Eugen.fritz.box>
2023-10-29 22:56:34 +0100

    additional ts file, not working yet

---
+++ translations/client_add_de.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="de_DE">
+<context>
+    <message>
+        <source>Open %1 locally</source>
+        <translation>%1 lokal öffnen</translation>
+    </message>
+    <message>
+        <location filename="../src/gui/tray/ActivityItem.qml" line="29"/>
+        <source>In %1</source>
+        <translation>In %1</translation>
+    </message>
+</context>
+</TS>
