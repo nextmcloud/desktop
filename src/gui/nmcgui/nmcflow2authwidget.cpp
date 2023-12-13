dcf1b241a3b352a22f32620c806cf260ca1c4105
Eugen Fischer <E.Fischer@external.t-systems.com>
2023-12-13 11:24:44 +0100

    Initial commit, classes created

---
+++ src/gui/nmcgui/nmcflow2authwidget.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) by Eugen Fischer
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include "nmcflow2authwidget.h"
+
+namespace OCC {
+
+
+NMCFlow2AuthWidget::NMCFlow2AuthWidget(QWidget *parent)
+    : Flow2AuthWidget(parent)
+{
+
+}
+
+
+} // namespace OCC
---
+++ src/gui/nmcgui/nmcowncloudadvancedsetuppage.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) by Klaas Freitag <freitag@owncloud.com>
+ * Copyright (C) by Krzesimir Nowak <krzesimir@endocode.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#ifndef MIRALL_NMCOWNCLOUD_ADVANCED_SETUP_PAGE_H
+#define MIRALL_NMCOWNCLOUD_ADVANCED_SETUP_PAGE_H
+
+#include "wizard/owncloudadvancedsetuppage.h"
+
+
+namespace OCC {
+
+/**
+ * @brief The NMCOwncloudAdvancedSetupPage class
+ * @ingroup gui
+ */
+
+class NMCOwncloudAdvancedSetupPage : public OwncloudAdvancedSetupPage
+{
+    Q_OBJECT
+
+public:
+    NMCOwncloudAdvancedSetupPage(OwncloudWizard *wizard);
+    ~NMCOwncloudAdvancedSetupPage() = default;
+};
+
+} // namespace OCC
+
+#endif
