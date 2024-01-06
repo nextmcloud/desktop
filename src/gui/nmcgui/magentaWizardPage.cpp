24ca5b0a134e8b83a5a92578ad771cbf63751c80
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-01-06 23:52:53 +0100

    Fixed pixmap size

--- src/gui/nmcgui/magentaWizardPage.cpp
+++ src/gui/nmcgui/magentaWizardPage.cpp
@@ -27,7 +27,8 @@ MagentaWizardPage::MagentaWizardPage(OwncloudWizard *ocWizard)
     _ui->hostYourOwnServerLabel->setVisible(false);
     _ui->loginButton->setVisible(false);
     _ui->createAccountButton->setVisible(false);
-    _ui->slideShow->startShow(2500);
+    _ui->slideShow->startShow();
+    _ui->slideShow->setInterval(2500);
 
     setFinalPage(true);
 }
@@ -47,13 +48,13 @@ void MagentaWizardPage::styleSlideShow()
     const auto theme = Theme::instance();
     const auto backgroundColor = palette().window().color();
 
-    const QString page1FileName = QString(":/client/theme/NMCIcons/page1.png");
-    const QString page2FileName = QString(":/client/theme/NMCIcons/page2.png");
-    const QString page3FileName = QString(":/client/theme/NMCIcons/page3.png");
+    QPixmap page1Pixmap = QString(":/client/theme/NMCIcons/page1.png");
+    QPixmap page2Pixmap = QString(":/client/theme/NMCIcons/page2.png");
+    QPixmap page3Pixmap = QString(":/client/theme/NMCIcons/page3.png");
 
-    _ui->slideShow->addSlide(page1FileName, tr("Page 1"));
-    _ui->slideShow->addSlide(page2FileName, tr("Page 2"));
-    _ui->slideShow->addSlide(page3FileName, tr("Page 3"));
+    _ui->slideShow->addSlide(page1Pixmap.scaled(page1Pixmap.size()*0.33, Qt::KeepAspectRatio,Qt::SmoothTransformation), tr("PAGE_1_WIZARD"));
+    _ui->slideShow->addSlide(page2Pixmap.scaled(page2Pixmap.size()*0.33, Qt::KeepAspectRatio,Qt::SmoothTransformation), tr("PAGE_2_WIZARD"));
+    _ui->slideShow->addSlide(page3Pixmap.scaled(page3Pixmap.size()*0.33, Qt::KeepAspectRatio,Qt::SmoothTransformation), tr("PAGE_3_WIZARD"));
 
     const auto isDarkBackground = Theme::isDarkColor(backgroundColor);
     _ui->slideShowNextButton->setIcon(theme->uiThemeIcon(QString("control-next.svg"), isDarkBackground));
