30526eab40cf25f76878f6d2280e8de7b8278932
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-01-16 11:07:18 +0100

    Settings icons and their size

--- src/gui/wizard/flow2authwidget.h
+++ src/gui/wizard/flow2authwidget.h
@@ -67,10 +67,12 @@ protected Q_SLOTS:
     void slotOpenBrowser();
     void slotCopyLinkToClipboard();
 
+protected:
+    virtual void customizeStyle();
+
 private:
     void startSpinner();
     void stopSpinner(bool showStatusLabel);
-    void customizeStyle();
     void setLogo();
 
     QProgressIndicator *_progressIndi;
