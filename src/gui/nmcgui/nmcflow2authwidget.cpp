c07b6307444d46f1dc9da446d5a872d991b9dd77
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-01-17 00:39:05 +0100

    New icons and size and margin fixes

--- src/gui/nmcgui/nmcflow2authwidget.cpp
+++ src/gui/nmcgui/nmcflow2authwidget.cpp
@@ -29,7 +29,7 @@ NMCFlow2AuthWidget::NMCFlow2AuthWidget(QWidget *parent)
     : Flow2AuthWidget(parent)
 {
 
-    setFixedSize(700,502);
+    //setFixedSize(700,502);
 
     getUi().copyLinkLabel->setVisible(false);
     getUi().openLinkLabel->setVisible(false);
@@ -45,7 +45,7 @@ NMCFlow2AuthWidget::NMCFlow2AuthWidget(QWidget *parent)
     });
 
     //Set login button size and style
-    QSize buttonSize(120,32);
+    QSize buttonSize(130,32);
     const QString styleSheetHoverPart = "QPushButton:hover { background-color: #c00063; }";
     const QString styleSheet("QPushButton{font-size: 15px; border: %1px solid; border-color: black; border-radius: 4px; background-color: %2; color: %3;}" + styleSheetHoverPart );
     loginBrowserButton->setStyleSheet(styleSheet.arg("0","#E20074","white"));
@@ -59,7 +59,7 @@ NMCFlow2AuthWidget::NMCFlow2AuthWidget(QWidget *parent)
 
     mainVerticalLayout->addLayout(subMainHorizontalLayout);
 
-    QSpacerItem *spacer4 = new QSpacerItem(32,1, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QSpacerItem *spacer4 = new QSpacerItem(1,1, QSizePolicy::Fixed, QSizePolicy::Fixed);
     subMainHorizontalLayout->addSpacerItem(spacer4);
     subMainHorizontalLayout->addLayout(leftSideVerticalLayout);
     subMainHorizontalLayout->addLayout(rightSideVerticalLayout);
@@ -68,11 +68,11 @@ NMCFlow2AuthWidget::NMCFlow2AuthWidget(QWidget *parent)
     auto hLogoAndLabelLayout = new QHBoxLayout(this);
     getUi().verticalLayout_3->removeWidget(getUi().logoLabel);
     getUi().logoLabel->setFixedSize(36,36);
-    getUi().logoLabel->setPixmap(QIcon(QLatin1String(":/client/theme/account.svg")).pixmap(36,36));
+    getUi().logoLabel->setPixmap(QIcon(QLatin1String(":/client/theme/NMCIcons/tlogocarrier.svg")).pixmap(36,36));
 
     hLogoAndLabelLayout->addWidget(getUi().logoLabel);
 
-    QSpacerItem *spacer3 = new QSpacerItem(1,64, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QSpacerItem *spacer3 = new QSpacerItem(1,32, QSizePolicy::Fixed, QSizePolicy::Fixed);
     leftSideVerticalLayout->addSpacerItem(spacer3);
 
     QSpacerItem *spacer9 = new QSpacerItem(8,1, QSizePolicy::Fixed, QSizePolicy::Fixed);
@@ -110,16 +110,22 @@ NMCFlow2AuthWidget::NMCFlow2AuthWidget(QWidget *parent)
     //Add buttons
     leftSideVerticalLayout->insertWidget(7, loginBrowserButton);
 
+    QSpacerItem *spacer11 = new QSpacerItem(1,1, QSizePolicy::Fixed, QSizePolicy::Expanding);
+    leftSideVerticalLayout->addSpacerItem(spacer11);
+
     //Add items to the right side
     QLabel *bigMagetnaIcon = new QLabel("Test");
     bigMagetnaIcon->setFixedSize(175,175);
-    bigMagetnaIcon->setPixmap(QIcon(QLatin1String(":/client/theme/account.svg")).pixmap(175,175));
+    bigMagetnaIcon->setPixmap(QIcon(QLatin1String(":/client/theme/NMCIcons/ApplicationLogo.svg")).pixmap(175,175));
 
-    QSpacerItem *spacer1 = new QSpacerItem(1,128, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QSpacerItem *spacer1 = new QSpacerItem(1,98, QSizePolicy::Fixed, QSizePolicy::Fixed);
     rightSideVerticalLayout->addSpacerItem(spacer1);
     rightSideVerticalLayout->addWidget(bigMagetnaIcon);
 
-    QSpacerItem *spacer2 = new QSpacerItem(48,1, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QSpacerItem *spacer10 = new QSpacerItem(1,1, QSizePolicy::Fixed, QSizePolicy::Expanding);
+    rightSideVerticalLayout->addSpacerItem(spacer10);
+
+    QSpacerItem *spacer2 = new QSpacerItem(0,1, QSizePolicy::Fixed, QSizePolicy::Fixed);
     subMainHorizontalLayout->addSpacerItem(spacer2);
 
 
