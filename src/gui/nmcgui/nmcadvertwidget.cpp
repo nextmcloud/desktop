ef1c2003ee3de714cee9fbb6a85fb95fd0f93012
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-01-17 15:44:16 +0100

    Advert window opening mechanism added

---
+++ src/gui/nmcgui/nmcadvertwidget.cpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) by Eugen Fischer
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ */
+
+#include "NMCAdvertWidget.h"
+
+NMCAdvertWidget::NMCAdvertWidget(QWidget *parent) : QWidget(parent)
+{
+    // Hier können Sie die Initialisierung Ihres Widgets durchführen
+}
