b50dd71212d331447f7782d23155af54b0ccfe96
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-01-22 15:06:27 +0100

    Changed window size and added docstrings

--- src/gui/nmcgui/nmcowncloudadvancedsetuppage.h
+++ src/gui/nmcgui/nmcowncloudadvancedsetuppage.h
@@ -43,13 +43,23 @@ public:
     ~NMCOwncloudAdvancedSetupPage() = default;
 
 protected:
+    /**
+     * @brief Override of paintEvent to handle custom painting.
+     * @param event Pointer to the QPaintEvent object.
+     */
     void paintEvent(QPaintEvent *event) override;
 
 private:
+    /**
+     * @brief Pointer to the QLabel for the custom logo.
+     */
     QLabel *_tLogoLbl = nullptr;
+
+    /**
+     * @brief Helper function to clean up elements.
+     */
     void cleanUpElements();
 };
-
 
 } // namespace OCC
 
