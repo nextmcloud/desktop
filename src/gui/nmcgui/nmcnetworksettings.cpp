5446e16269f7a22a8bc43a2d1c7287fdcc446cba
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-01-22 22:43:30 +0100

    fixed all layouts, added docstrings

--- src/gui/nmcgui/nmcnetworksettings.cpp
+++ src/gui/nmcgui/nmcnetworksettings.cpp
@@ -22,141 +22,88 @@ namespace OCC {
 NMCNetworkSettings::NMCNetworkSettings(QWidget *parent)
     : NetworkSettings(parent)
 {
-    setDefaultSettings();
     setLayout();
-    setLogic();
-}
-
-void NMCNetworkSettings::setDefaultSettings()
-{
-    //Set default settings
-    getUi()->downloadSpinBox->setVisible(true);
-    if(getUi()->downloadLimitRadioButton->isChecked())
-    {
-        getUi()->downloadSpinBox->setEnabled(true);
-    }
-    else{
-        getUi()->downloadSpinBox->setEnabled(false);
-    }
-    getUi()->downloadSpinBoxLabel->setVisible(true);
-    getUi()->uploadSpinBox->setVisible(true);
-    if(getUi()->uploadLimitRadioButton->isChecked())
-    {
-        getUi()->uploadSpinBox->setEnabled(true);
-    }
-    else{
-        getUi()->uploadSpinBox->setEnabled(false);
-    }
-    getUi()->uploadSpinBoxLabel->setVisible(true);
 }
 
 void NMCNetworkSettings::setLayout()
 {
     //Fix Layouts
     //Proxy settings
-    // getUi()->manualSettings->layout()->removeItem(getUi()->horizontalLayout_8);
-    // auto *spinBoxWidget = new QWidget(this);
-    // spinBoxWidget->setEnabled(false);
-    // spinBoxWidget->setLayout(getUi()->horizontalLayout_8);
-    // getUi()->horizontalLayout_8->setContentsMargins(0,0,0,0);
-    // static_cast<QGridLayout *>(getUi()->proxyGroupBox->layout())->addWidget(spinBoxWidget, 2, 1);
-    // disconnect(getUi()->manualProxyRadioButton, &QAbstractButton::toggled, getUi()->manualSettings, &QWidget::setVisible);
-    // connect(getUi()->manualProxyRadioButton, &QAbstractButton::toggled, this, [this, spinBoxWidget](bool checked){
-    //     getUi()->manualSettings->setEnabled(checked);
-    //     spinBoxWidget->setEnabled(checked);
-    // });
+    getUi()->proxyGroupBox->setTitle("");
+    getUi()->proxyGroupBox->layout()->removeWidget(getUi()->manualProxyRadioButton);
+    getUi()->proxyGroupBox->layout()->removeWidget(getUi()->noProxyRadioButton);
+    getUi()->proxyGroupBox->layout()->removeWidget(getUi()->systemProxyRadioButton);
+    getUi()->proxyGroupBox->layout()->removeItem(getUi()->horizontalLayout_7);
+    getUi()->proxyGroupBox->layout()->removeItem(getUi()->horizontalSpacer_2);
+    getUi()->proxyGroupBox->layout()->setContentsMargins(16,16,16,16);
+    getUi()->proxyGroupBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
 
-    // getUi()->manualSettings->setContentsMargins(0,0,0,0);
-    // getUi()->manualSettings->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-    // getUi()->horizontalSpacer_2->changeSize(0,0, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QGridLayout *proxyLayout = static_cast<QGridLayout *>(getUi()->proxyGroupBox->layout());
+    auto proxyLabel = new QLabel(tr("PROXY_SETTINGS"));
+    proxyLabel->setStyleSheet("QLabel{font-size: 12px; font-weight: bold;}");
 
-    //Remove spacer, so the elements can expand.
+    proxyLayout->addWidget(proxyLabel, 0, 0 );
+    proxyLayout->addItem(new QSpacerItem(1,8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
+    proxyLayout->addWidget(getUi()->noProxyRadioButton, 2, 0 );
+    proxyLayout->addWidget(getUi()->systemProxyRadioButton, 3, 0 );
+    proxyLayout->addWidget(getUi()->manualProxyRadioButton, 4, 0 );
+    proxyLayout->addLayout(getUi()->horizontalLayout_7, 5, 0);
+
+    //Remove the spacer, so the elements can expand.
     getUi()->horizontalSpacer->changeSize(0,0, QSizePolicy::Fixed, QSizePolicy::Fixed);
 
     //DownloadBox
     getUi()->verticalSpacer_2->changeSize(0,0, QSizePolicy::Fixed, QSizePolicy::Fixed);
-    getUi()->downloadBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-    getUi()->horizontalLayout_3->setSpacing(5);  
+    getUi()->downloadBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+    getUi()->horizontalLayout_3->setSpacing(8);
     getUi()->downloadBox->setTitle("");
     getUi()->downloadBox->layout()->removeWidget(getUi()->noDownloadLimitRadioButton);
     getUi()->downloadBox->layout()->removeWidget(getUi()->autoDownloadLimitRadioButton);
     getUi()->downloadBox->layout()->removeWidget(getUi()->downloadLimitRadioButton);
     getUi()->downloadBox->layout()->removeItem(getUi()->horizontalLayout_3);
+    getUi()->downloadBox->layout()->setContentsMargins(16,16,16,16);
+    getUi()->downloadBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
 
     QGridLayout *downLayout = static_cast<QGridLayout *>(getUi()->downloadBox->layout());
 
-    downLayout->addWidget(new QLabel("Download-Bandbreite"), 0, 0 );
-    downLayout->addWidget(getUi()->noDownloadLimitRadioButton, 1, 0 );
-    downLayout->addWidget(getUi()->autoDownloadLimitRadioButton, 2, 0 );
-    downLayout->addWidget(getUi()->downloadLimitRadioButton, 3, 0 );
-    downLayout->addItem(getUi()->horizontalLayout_3, 3, 1);
+    auto downLabel = new QLabel(tr("DOWNLOAD_BANDWITH"));
+    downLabel->setStyleSheet("QLabel{font-size: 12px; font-weight: bold;}");
+    downLayout->addWidget(downLabel, 0, 0 );
+    downLayout->addItem(new QSpacerItem(1,8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
+    downLayout->addWidget(getUi()->noDownloadLimitRadioButton, 2, 0 );
+    downLayout->addWidget(getUi()->autoDownloadLimitRadioButton, 3, 0 );
+    downLayout->addWidget(getUi()->downloadLimitRadioButton, 4, 0 );
+    downLayout->addItem(getUi()->horizontalLayout_3, 4, 1);
+
+    getUi()->downloadLimitRadioButton->setFixedHeight(getUi()->downloadSpinBox->height());
 
     //UploadBox
     getUi()->uploadBox->layout()->removeItem(getUi()->horizontalLayout_4);
     static_cast<QGridLayout *>(getUi()->uploadBox->layout())->addItem(getUi()->horizontalLayout_4, 2, 1);
 
     getUi()->verticalSpacer_3->changeSize(0,0, QSizePolicy::Fixed, QSizePolicy::Fixed);
-    getUi()->uploadBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
-    getUi()->horizontalLayout_4->setSpacing(5);
+    getUi()->uploadBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+    getUi()->horizontalLayout_4->setSpacing(8);
     getUi()->uploadBox->setTitle("");
     getUi()->uploadBox->layout()->removeWidget(getUi()->noUploadLimitRadioButton);
     getUi()->uploadBox->layout()->removeWidget(getUi()->autoUploadLimitRadioButton);
     getUi()->uploadBox->layout()->removeWidget(getUi()->uploadLimitRadioButton);
     getUi()->uploadBox->layout()->removeItem(getUi()->horizontalLayout_4);
+    getUi()->uploadBox->layout()->setContentsMargins(16,16,16,16);
+    getUi()->uploadBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
 
     QGridLayout *upLayout = static_cast<QGridLayout *>(getUi()->uploadBox->layout());
 
-    upLayout->addWidget(new QLabel("Upload-Bandbreite"), 0, 0 );
-    upLayout->addWidget(getUi()->noUploadLimitRadioButton, 1, 0 );
-    upLayout->addWidget(getUi()->autoUploadLimitRadioButton, 2, 0 );
-    upLayout->addWidget(getUi()->uploadLimitRadioButton, 3, 0 );
-    upLayout->addItem(getUi()->horizontalLayout_4, 3, 1);
+    auto uploadLabel = new QLabel(tr("UPLOAD_BANDWITH"));
+    uploadLabel->setStyleSheet("QLabel{font-size: 12px; font-weight: bold;}");
+    upLayout->addWidget(uploadLabel, 0, 0 );
+    upLayout->addItem(new QSpacerItem(1,8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
+    upLayout->addWidget(getUi()->noUploadLimitRadioButton, 2, 0 );
+    upLayout->addWidget(getUi()->autoUploadLimitRadioButton, 3, 0 );
+    upLayout->addWidget(getUi()->uploadLimitRadioButton, 4, 0 );
+    upLayout->addItem(getUi()->horizontalLayout_4, 4, 1);
 
-    //Fix widgets visibility
-    //Download settings
-    connect(getUi()->noDownloadLimitRadioButton, &QAbstractButton::clicked, this, [this](){
-        getUi()->downloadSpinBox->setVisible(true);
-        getUi()->downloadSpinBoxLabel->setVisible(true);
-        getUi()->downloadSpinBox->setEnabled(false);
-    });
-    connect(getUi()->autoDownloadLimitRadioButton, &QAbstractButton::clicked, this, [this](){
-        getUi()->downloadSpinBox->setVisible(true);
-        getUi()->downloadSpinBoxLabel->setVisible(true);
-        getUi()->downloadSpinBox->setEnabled(false);
-    });
-    connect(getUi()->downloadLimitRadioButton, &QAbstractButton::clicked, this, [this](){
-        getUi()->downloadSpinBox->setVisible(true);
-        getUi()->downloadSpinBoxLabel->setVisible(true);
-        getUi()->downloadSpinBox->setEnabled(true);
-    });
-
-
-    //Upload settings
-    connect(getUi()->noUploadLimitRadioButton, &QAbstractButton::clicked, this, [this](){
-        getUi()->uploadSpinBox->setVisible(true);
-        getUi()->uploadSpinBoxLabel->setVisible(true);
-        getUi()->uploadSpinBox->setEnabled(false);
-    });
-    connect(getUi()->autoUploadLimitRadioButton, &QAbstractButton::clicked, this, [this](){
-        getUi()->uploadSpinBox->setVisible(true);
-        getUi()->uploadSpinBoxLabel->setVisible(true);
-        getUi()->uploadSpinBox->setEnabled(false);
-    });
-    connect(getUi()->uploadLimitRadioButton, &QAbstractButton::clicked, this, [this](){
-        getUi()->uploadSpinBox->setVisible(true);
-        getUi()->uploadSpinBoxLabel->setVisible(true);
-        getUi()->uploadSpinBox->setEnabled(true);
-    });
-
-    //Fix initial proxy warning, it shows a red border at initial state whenever the widget becomes visible
-    //Override the nextcloud stylesheet
-    getUi()->hostLineEdit->setStyleSheet(QString());
+    getUi()->uploadLimitRadioButton->setFixedHeight(getUi()->uploadSpinBox->height());
 }
-
-void NMCNetworkSettings::setLogic()
-{
-
-}
-
 
 } // namespace OCC
