6305c0e394880ddad1fb5bc77f513fc54708f5ce
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-01-24 18:50:22 +0100

    Live-Backup button and Titel etc

--- src/gui/accountsettings.cpp
+++ src/gui/accountsettings.cpp
@@ -1114,7 +1114,7 @@ void AccountSettings::showConnectionLabel(const QString &message, QStringList er
         _ui->connectLabel->setToolTip({});
         _ui->connectLabel->setStyleSheet(errStyle);
     }
-    _ui->accountStatus->setVisible(!message.isEmpty());
+    //_ui->accountStatus->setVisible(!message.isEmpty()); //NMC Customization
 }
 
 void AccountSettings::slotEnableCurrentFolder(bool terminate)
