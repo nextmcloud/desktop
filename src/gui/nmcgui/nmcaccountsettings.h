93fb9e8e2153c7c12940ba9ed4f9aa26222aea25
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-01-24 18:50:22 +0100

    Live-Backup button and Titel etc

--- src/gui/nmcgui/nmcaccountsettings.h
+++ src/gui/nmcgui/nmcaccountsettings.h
@@ -15,9 +15,48 @@
 #ifndef MIRALL_ACCOUNTSETTINGSMAGENTA_H
 #define MIRALL_ACCOUNTSETTINGSMAGENTA_H
 
+#include "QtWidgets/qpushbutton.h"
 #include <accountsettings.h>
+#include <QPushButton>
+#include <QIcon>
+#include <QPainter>
 
 namespace OCC {
+
+class CustomButton : public QPushButton {
+public:
+    explicit CustomButton(const QString &text, const QIcon &icon, QWidget *parent = nullptr)
+        : QPushButton(text, parent)
+        , icon_(icon)
+    {
+        setIconSize(QSize(24, 24));
+    }
+
+    void setleftIconMargin(int margin){
+        m_leftMargin = margin;
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event) override {
+        QPushButton::paintEvent(event);
+
+        if (!icon_.isNull()) {
+            QRect iconRect = QRect( m_leftMargin, //left margin
+                                   (height() - iconSize().height()) / 2, // vertical center
+                                   iconSize().width(),
+                                   iconSize().height());
+
+            QPainter painter(this);
+            painter.drawPixmap(iconRect, icon_.pixmap(iconSize()));
+        }
+    }
+
+private:
+    QIcon icon_;
+    int m_leftMargin = 4;
+};
+
+
 
 /**
  * @brief The NMCAccountSettingsMagenta class
@@ -34,6 +73,12 @@ public:
 protected:
     void setDefaultSettings();
     void setLayout();
+
+private:
+    CustomButton *m_liveAccountButton = nullptr;
+    QLabel *m_liveTitle = nullptr;
+    QLabel *m_liveDescription = nullptr;
+    QLabel *m_folderSync = nullptr;
 };
 
 
