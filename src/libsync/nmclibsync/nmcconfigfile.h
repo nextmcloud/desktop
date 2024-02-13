f0128367d0b29ab2ba83b189127326cc9487fed7
Eugen Fischer <E.Fischer@external.t-systems.com>
2024-02-13 10:35:52 +0100

    Improved docstrings

--- src/libsync/nmclibsync/nmcconfigfile.h
+++ src/libsync/nmclibsync/nmcconfigfile.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) by Daniel Molkentin <danimo@owncloud.com>
+ * Copyright (C) by Eugen Fischer
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,22 +20,44 @@
 namespace OCC {
 
 /**
- * @brief The NMCConfigFile class
+ * @brief The NMCConfigFile class.
  * @ingroup lib
+ *
+ * Subclass of ConfigFile representing the configuration file for NMC (MagentaCustomization) in the OwnCloud Sync library.
  */
 class OWNCLOUDSYNC_EXPORT NMCConfigFile : public ConfigFile
 {
 public:
+    /**
+     * @brief Default constructor for NMCConfigFile.
+     */
     explicit NMCConfigFile() = default;
+
+    /**
+     * @brief Default destructor for NMCConfigFile.
+     */
     ~NMCConfigFile() = default;
 
-    // MagentaCustomization
+    /**
+     * @brief Check if transferring usage data is enabled.
+     *
+     * @param connection Optional parameter specifying the connection; default is an empty string.
+     * @return True if transferring usage data is enabled, false otherwise.
+     */
     [[nodiscard]] bool transferUsageData(const QString &connection = QString()) const;
+
+    /**
+     * @brief Set the status of transferring usage data.
+     *
+     * @param usageData True to enable transferring usage data, false to disable.
+     * @param connection Optional parameter specifying the connection; default is an empty string.
+     */
     void setTransferUsageData(bool usageData, const QString &connection);
 
 private:
-    QString m_transferUsageData = "TransferUsageData";
+    QString m_transferUsageData = "TransferUsageData"; ///< Configuration key for storing the status of transferring usage data.
 };
+
 
 
 } // namespace OCC
