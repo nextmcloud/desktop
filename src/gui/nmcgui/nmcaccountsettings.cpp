7a1859024fe567d93c993492b3f5e40b25ce91f4
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2024-07-31 10:06:58 +0200

    fix for percentage

--- src/gui/nmcgui/nmcaccountsettings.cpp
+++ src/gui/nmcgui/nmcaccountsettings.cpp
@@ -97,7 +97,7 @@ void NMCAccountSettings::setLayout()
     auto *quotaVLayout = new QVBoxLayout(this);
     quotaVLayout->setSpacing(4);
     auto *quota = new QLabel(this);
-    // quota->setText(QCoreApplication::translate("", "USED_STORAGE_%1").arg(getUi()->quotaProgressBar->value() > 0 ? Utility::compactFormatDouble(getUi()->quotaProgressBar->value(), 1) : QString::number(0)));
+    quota->setText(QCoreApplication::translate("", "USED_STORAGE_%1").arg(Utility::compactFormatDouble(getUi()->quotaProgressBar->value(), 1)));
 
     quotaVLayout->addSpacerItem(new QSpacerItem(1,12, QSizePolicy::Fixed, QSizePolicy::Fixed));
     quotaVLayout->addWidget(getUi()->quotaInfoLabel);
