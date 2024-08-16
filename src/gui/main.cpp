d1ad529a4a3a9128afecaccf1c605e7ebddd3a71
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2024-08-16 07:52:43 +0200

    solve merge conflicts

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -61,12 +61,9 @@ int main(int argc, char **argv)
 
     bool resourceLoaded = false;
     const QString currentPath = QDir::currentPath();
-    if(Utility::isMac())
-    {
+    if(Utility::isMac()) {
         resourceLoaded = QResource::registerResource(QDir::toNativeSeparators("/Applications/MagentaCLOUD.app/Contents/Resources/nmctheme_v1.rcc"));
-    }
-    else if(Utility::isWindows() || !resourceLoaded)
-    {
+    } else if(Utility::isWindows() || !resourceLoaded) {
         resourceLoaded = QResource::registerResource(QDir::toNativeSeparators(currentPath + "/nmctheme_v1.rcc"));
     }
 
