e608d8cbd563eeacdf51c4790f81b6539a589586
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2024-10-28 10:14:55 +0100

    welcome popup wizard changes

--- src/gui/owncloudgui.h
+++ src/gui/owncloudgui.h
@@ -29,6 +29,8 @@
 #include <QDBusConnection>
 #endif
 
+#include "nmcgui/nmcadvertwidget.h"
+
 namespace OCC {
 
 class Folder;
@@ -127,6 +129,7 @@ private:
 
     QList<QAction *> _recentItemsActions;
     Application *_app;
+    NMCAdvertWidget *_advertWidget = nullptr;
 };
 
 } // namespace OCC
