d76033f82cfed8244f3438296b91a9e76f57b8b4
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-02-13 14:02:49 +0100

    solve merge conflicts

--- src/gui/socketapi/socketapi.cpp
+++ src/gui/socketapi/socketapi.cpp
@@ -1206,41 +1206,14 @@ void SocketApi::sendSharingContextMenuOptions(const FileData &fileData, SocketLi
     // If sharing is globally disabled, do not show any sharing entries.
     // If there is no permission to share for this file, add a disabled entry saying so
     // if (isOnTheServer && !record._remotePerm.isNull() && !record._remotePerm.hasPermission(RemotePermissions::CanReshare)) {
-    //     listener->sendMessage(QLatin1String("MENU_ITEM:DISABLED:d:") + (!record.isDirectory() ? tr("Resharing this file is not allowed") : tr("Resharing this folder is not allowed")));
+        // listener->sendMessage(QLatin1String("MENU_ITEM:DISABLED:d:") + (!record.isDirectory() ? tr("Resharing this file is not allowed") : tr("Resharing this folder is not allowed")));
     // } else {
-    //     listener->sendMessage(QLatin1String("MENU_ITEM:SHARE") + flagString + tr("Share options"));
-
-    //     // Do we have public links?
-    //     bool publicLinksEnabled = theme->linkSharing() && capabilities.sharePublicLink();
-
-    //     // Is is possible to create a public link without user choices?
-    //     bool canCreateDefaultPublicLink = publicLinksEnabled
-    //         && !capabilities.sharePublicLinkEnforceExpireDate()
-    //         && !capabilities.sharePublicLinkAskOptionalPassword()
-    //         && !capabilities.sharePublicLinkEnforcePassword();
-
-    //     if (canCreateDefaultPublicLink) {
-    //         if (isSecureFileDropSupported) {
-    //             listener->sendMessage(QLatin1String("MENU_ITEM:COPY_SECUREFILEDROP_LINK") + QLatin1String("::") + tr("Copy secure file drop link"));
-    //         } else {
-    //             listener->sendMessage(QLatin1String("MENU_ITEM:COPY_PUBLIC_LINK") + flagString + tr("Copy public link"));
-    //         }
-    //     } else if (publicLinksEnabled) {
-    //         if (isSecureFileDropSupported) {
-    //             listener->sendMessage(QLatin1String("MENU_ITEM:MANAGE_PUBLIC_LINKS") + QLatin1String("::") + tr("Copy secure filedrop link"));
-    //         } else {
-    //             listener->sendMessage(QLatin1String("MENU_ITEM:MANAGE_PUBLIC_LINKS") + flagString + tr("Copy public link"));
-    //         }
-    //     }
+        // listener->sendMessage(QLatin1String("MENU_ITEM:SHARE") + flagString + tr("Share options"));
     // }
 
     // if (itemEncryptionFlag == SharingContextItemEncryptedFlag::NotEncryptedItem) {
-    //     listener->sendMessage(QLatin1String("MENU_ITEM:COPY_PRIVATE_LINK") + flagString + tr("Copy internal link"));
+        // listener->sendMessage(QLatin1String("MENU_ITEM:COPY_PRIVATE_LINK") + flagString + tr("Copy internal link"));
     // }
-
-    // Disabled: only providing email option for private links would look odd,
-    // and the copy option is more general.
-    //listener->sendMessage(QLatin1String("MENU_ITEM:EMAIL_PRIVATE_LINK") + flagString + tr("Send private link by email …"));
 }
 
 void SocketApi::sendEncryptFolderCommandMenuEntries(const QFileInfo &fileInfo,
