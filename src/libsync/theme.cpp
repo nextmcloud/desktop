8a351fcf1217884127d33447838901f27fbaf8a8
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-03-04 14:32:58 +0100

    dark mode disabled changes

--- src/libsync/theme.cpp
+++ src/libsync/theme.cpp
@@ -865,7 +865,8 @@ double Theme::getColorDarkness(const QColor &color)
 
 bool Theme::isDarkColor(const QColor &color)
 {
-    return getColorDarkness(color) > 0.5;
+    Q_UNUSED(color)
+    return false;
 }
 
 QColor Theme::getBackgroundAwareLinkColor(const QColor &backgroundColor)
@@ -1012,16 +1013,6 @@ bool Theme::darkMode() const
 {
     connectToPaletteSignal();
     const auto isDarkFromStyle = [] {
-        switch (qGuiApp->styleHints()->colorScheme())
-        {
-        case Qt::ColorScheme::Dark:
-            return true;
-        case Qt::ColorScheme::Light:
-            return false;
-        case Qt::ColorScheme::Unknown:
-            return Theme::isDarkColor(QGuiApplication::palette().window().color());
-        }
-
         return false;
     };
 
