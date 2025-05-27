5022633912de22c06bcab02287710d8fcbe0da7d
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-05-27 10:12:49 +0200

    hide checkbox chat

--- src/gui/nmcgui/nmcgeneralsettings.cpp
+++ src/gui/nmcgui/nmcgeneralsettings.cpp
@@ -57,14 +57,14 @@ void NMCGeneralSettings::setNMCLayout()
     //
     const QString commonStyles = R"(
         QCheckBox:checked { color: #e20074; }
-        QPushButton { height: 32px; width: 180px; border: 1px solid black; background-color: #ededed; font-size: 13px; border-radius: 4px; }
+        QPushButton { height: 32px; width: 200px; border: 1px solid black; background-color: #ededed; font-size: 13px; border-radius: 4px; }
         QPushButton::hover { background-color: white; }
     )";
 
     //General settings
     auto generalSettingsLabel = new QLabel(QCoreApplication::translate("", "GENERAL_SETTINGS"));
     generalSettingsLabel->setStyleSheet("QLabel{font-size: 12px; font-weight: bold;}");
-    getUi()->generalGroupBox->setTitle("");
+    getUi()->chatNotificationsCheckBox->hide();
     getUi()->generalGroupBox->layout()->removeWidget(getUi()->chatNotificationsCheckBox);
     getUi()->generalGroupBox->layout()->removeWidget(getUi()->serverNotificationsCheckBox);
     getUi()->generalGroupBox->layout()->removeWidget(getUi()->autostartCheckBox);
