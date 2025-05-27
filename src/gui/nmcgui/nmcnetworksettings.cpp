89efdb46deb13272136c26773c9597c1cf1a972d
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-05-27 07:48:52 +0200

    update settings view

--- src/gui/nmcgui/nmcnetworksettings.cpp
+++ src/gui/nmcgui/nmcnetworksettings.cpp
@@ -30,94 +30,100 @@ NMCNetworkSettings::NMCNetworkSettings(const AccountPtr &account, QWidget *paren
     setLayout();
 }
 
-    void NMCNetworkSettings::setLayout()
-    {
-        // Proxy Settings
-        auto *ui = getUi();
+void NMCNetworkSettings::setLayout()
+{
+    auto *ui = getUi();
 
-        ui->proxyGroupBox->setTitle({});
-        ui->proxyGroupBox->layout()->removeWidget(ui->manualProxyRadioButton);
-        ui->proxyGroupBox->layout()->removeWidget(ui->noProxyRadioButton);
-        ui->proxyGroupBox->layout()->removeWidget(ui->systemProxyRadioButton);
-        ui->proxyGroupBox->layout()->removeItem(ui->horizontalLayout_7);
-        ui->proxyGroupBox->layout()->removeItem(ui->horizontalSpacer_2);
-        ui->proxyGroupBox->layout()->setContentsMargins(16, 16, 16, 16);
-        ui->proxyGroupBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
+    //
+    // Proxy Settings
+    //
+    ui->proxyGroupBox->setTitle({});
+    ui->proxyGroupBox->layout()->removeWidget(ui->manualProxyRadioButton);
+    ui->proxyGroupBox->layout()->removeWidget(ui->noProxyRadioButton);
+    ui->proxyGroupBox->layout()->removeWidget(ui->systemProxyRadioButton);
+    ui->proxyGroupBox->layout()->removeItem(ui->horizontalLayout_7);
+    ui->proxyGroupBox->layout()->removeItem(ui->horizontalSpacer_2);
+    ui->proxyGroupBox->layout()->setContentsMargins(16, 16, 16, 16);
+    ui->proxyGroupBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
 
-        auto *proxyLayout = qobject_cast<QGridLayout *>(ui->proxyGroupBox->layout());
-        auto proxyLabel = new QLabel(QCoreApplication::translate("", "PROXY_SETTINGS"));
-        proxyLabel->setStyleSheet("QLabel { font-size: 12px; font-weight: bold; }");
+    auto *proxyLayout = qobject_cast<QGridLayout *>(ui->proxyGroupBox->layout());
+    auto *proxyLabel = new QLabel(QCoreApplication::translate("", "PROXY_SETTINGS"));
+    proxyLabel->setStyleSheet("QLabel { font-size: 12px; font-weight: bold; }");
 
-        proxyLayout->addWidget(proxyLabel, 0, 0);
-        proxyLayout->addItem(new QSpacerItem(1, 8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
-        proxyLayout->addWidget(ui->noProxyRadioButton, 2, 0);
-        proxyLayout->addWidget(ui->systemProxyRadioButton, 3, 0);
-        proxyLayout->addWidget(ui->manualProxyRadioButton, 4, 0);
-        proxyLayout->addLayout(ui->horizontalLayout_7, 5, 0);
+    proxyLayout->addWidget(proxyLabel, 0, 0);
+    proxyLayout->addItem(new QSpacerItem(1, 8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
+    proxyLayout->addWidget(ui->noProxyRadioButton, 2, 0);
+    proxyLayout->addWidget(ui->systemProxyRadioButton, 3, 0);
+    proxyLayout->addWidget(ui->manualProxyRadioButton, 4, 0);
+    proxyLayout->addLayout(ui->horizontalLayout_7, 5, 0);
 
-        proxyLayout->setStyleSheet("QRadioButton:checked { color: #e20074; }");
-        proxyLayout->setStyleSheet("QSpinBox { padding: 2px; }");
+    ui->horizontalSpacer->changeSize(0, 0, QSizePolicy::Fixed, QSizePolicy::Fixed);
 
-        ui->horizontalSpacer->changeSize(0, 0, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    //
+    // Download Bandwidth
+    //
+    ui->verticalSpacer_2->changeSize(0, 0, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    ui->downloadBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+    ui->horizontalLayout_3->setSpacing(8);
+    ui->downloadBox->setTitle({});
+    ui->downloadBox->layout()->removeWidget(ui->noDownloadLimitRadioButton);
+    ui->downloadBox->layout()->removeWidget(ui->autoDownloadLimitRadioButton);
+    ui->downloadBox->layout()->removeWidget(ui->downloadLimitRadioButton);
+    ui->downloadBox->layout()->removeItem(ui->horizontalLayout_3);
+    ui->downloadBox->layout()->setContentsMargins(16, 16, 16, 16);
+    ui->downloadBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
 
-        // Download Box
-        ui->verticalSpacer_2->changeSize(0, 0, QSizePolicy::Fixed, QSizePolicy::Fixed);
-        ui->downloadBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-        ui->horizontalLayout_3->setSpacing(8);
-        ui->downloadBox->setTitle({});
-        ui->downloadBox->layout()->removeWidget(ui->noDownloadLimitRadioButton);
-        ui->downloadBox->layout()->removeWidget(ui->autoDownloadLimitRadioButton);
-        ui->downloadBox->layout()->removeWidget(ui->downloadLimitRadioButton);
-        ui->downloadBox->layout()->removeItem(ui->horizontalLayout_3);
-        ui->downloadBox->layout()->setContentsMargins(16, 16, 16, 16);
-        ui->downloadBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
+    auto *downLayout = qobject_cast<QGridLayout *>(ui->downloadBox->layout());
+    auto *downLabel = new QLabel(QCoreApplication::translate("", "DOWNLOAD_BANDWIDTH"));
+    downLabel->setStyleSheet("QLabel { font-size: 12px; font-weight: bold; }");
 
-        auto *downLayout = qobject_cast<QGridLayout *>(ui->downloadBox->layout());
-        auto downLabel = new QLabel(QCoreApplication::translate("", "DOWNLOAD_BANDWIDTH"));
-        downLabel->setStyleSheet("QLabel { font-size: 12px; font-weight: bold; }");
+    downLayout->addWidget(downLabel, 0, 0);
+    downLayout->addItem(new QSpacerItem(1, 8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
+    downLayout->addWidget(ui->noDownloadLimitRadioButton, 2, 0);
+    downLayout->addWidget(ui->autoDownloadLimitRadioButton, 3, 0);
+    downLayout->addWidget(ui->downloadLimitRadioButton, 4, 0);
+    downLayout->addLayout(ui->horizontalLayout_3, 4, 1);
 
-        downLayout->addWidget(downLabel, 0, 0);
-        downLayout->addItem(new QSpacerItem(1, 8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
-        downLayout->addWidget(ui->noDownloadLimitRadioButton, 2, 0);
-        downLayout->addWidget(ui->autoDownloadLimitRadioButton, 3, 0);
-        downLayout->addWidget(ui->downloadLimitRadioButton, 4, 0);
-        downLayout->addItem(ui->horizontalLayout_3, 4, 1);
+    ui->downloadLimitRadioButton->setFixedHeight(ui->downloadSpinBox->height());
 
-        downLayout->setStyleSheet("QRadioButton:checked { color: #e20074; }");
-        downLayout->setStyleSheet("QSpinBox { padding: 2px; }");
+    //
+    // Upload Bandwidth
+    //
+    ui->verticalSpacer_3->changeSize(0, 0, QSizePolicy::Fixed, QSizePolicy::Fixed);
+    ui->uploadBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+    ui->horizontalLayout_4->setSpacing(8);
+    ui->uploadBox->setTitle({});
+    ui->uploadBox->layout()->removeWidget(ui->noUploadLimitRadioButton);
+    ui->uploadBox->layout()->removeWidget(ui->autoUploadLimitRadioButton);
+    ui->uploadBox->layout()->removeWidget(ui->uploadLimitRadioButton);
+    ui->uploadBox->layout()->removeItem(ui->horizontalLayout_4);
+    ui->uploadBox->layout()->setContentsMargins(16, 16, 16, 16);
+    ui->uploadBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
 
-        ui->downloadLimitRadioButton->setFixedHeight(ui->downloadSpinBox->height());
+    auto *upLayout = qobject_cast<QGridLayout *>(ui->uploadBox->layout());
+    auto *upLabel = new QLabel(QCoreApplication::translate("", "UPLOAD_BANDWIDTH"));
+    upLabel->setStyleSheet("QLabel { font-size: 12px; font-weight: bold; }");
 
-        // Upload Box
-        ui->uploadBox->layout()->removeItem(ui->horizontalLayout_4);
-        qobject_cast<QGridLayout *>(ui->uploadBox->layout())->addItem(ui->horizontalLayout_4, 2, 1);
+    upLayout->addWidget(upLabel, 0, 0);
+    upLayout->addItem(new QSpacerItem(1, 8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
+    upLayout->addWidget(ui->noUploadLimitRadioButton, 2, 0);
+    upLayout->addWidget(ui->autoUploadLimitRadioButton, 3, 0);
+    upLayout->addWidget(ui->uploadLimitRadioButton, 4, 0);
+    upLayout->addLayout(ui->horizontalLayout_4, 4, 1);
 
-        ui->verticalSpacer_3->changeSize(0, 0, QSizePolicy::Fixed, QSizePolicy::Fixed);
-        ui->uploadBox->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
-        ui->horizontalLayout_4->setSpacing(8);
-        ui->uploadBox->setTitle({});
-        ui->uploadBox->layout()->removeWidget(ui->noUploadLimitRadioButton);
-        ui->uploadBox->layout()->removeWidget(ui->autoUploadLimitRadioButton);
-        ui->uploadBox->layout()->removeWidget(ui->uploadLimitRadioButton);
-        ui->uploadBox->layout()->removeItem(ui->horizontalLayout_4);
-        ui->uploadBox->layout()->setContentsMargins(16, 16, 16, 16);
-        ui->uploadBox->setStyleSheet("QGroupBox { background-color: white; border-radius: 4px; }");
+    ui->uploadLimitRadioButton->setFixedHeight(ui->uploadSpinBox->height());
 
-        auto *upLayout = qobject_cast<QGridLayout *>(ui->uploadBox->layout());
-        auto upLabel = new QLabel(QCoreApplication::translate("", "DOWNLOAD_BANDWIDTH"));
-        upLabel->setStyleSheet("QLabel { font-size: 12px; font-weight: bold; }");
+    //
+    // Styles
+    //
+    const QString commonStyles = R"(
+        QRadioButton:checked { color: #e20074; }
+        QSpinBox { padding: 2px; }
+    )";
 
-        upLayout->addWidget(upLabel, 0, 0);
-        upLayout->addItem(new QSpacerItem(1, 8, QSizePolicy::Fixed, QSizePolicy::Fixed), 1, 0);
-        upLayout->addWidget(ui->noUploadLimitRadioButton, 2, 0);
-        upLayout->addWidget(ui->autoUploadLimitRadioButton, 3, 0);
-        upLayout->addWidget(ui->uploadLimitRadioButton, 4, 0);
-        upLayout->addItem(ui->horizontalLayout_4, 4, 1);
-
-        upLayout->setStyleSheet("QRadioButton:checked { color: #e20074; }");
-        upLayout->setStyleSheet("QSpinBox { padding: 2px; }");
-
-        ui->uploadLimitRadioButton->setFixedHeight(ui->uploadSpinBox->height());
-    }
+    proxyLayout->setStyleSheet(commonStyles);
+    downLayout->setStyleSheet(commonStyles);
+    upLayout->setStyleSheet(commonStyles);
+}
 
 } // namespace OCC 
