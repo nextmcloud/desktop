ddd10ab73b16aa8655415e7b587d8ef3fcf56686
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-05-28 09:22:24 +0200

    radio border

--- src/gui/nmcgui/nmcnetworksettings.cpp
+++ src/gui/nmcgui/nmcnetworksettings.cpp
@@ -44,6 +44,7 @@ void NMCNetworkSettings::setLayout()
                 fx:0.5, fy:0.5,
                 stop:0 #e20074, stop:0.5 #e20074, stop:1 #ffffff
             );
+            border: 1px solid #e20074;
         }
     
         QRadioButton:checked {
