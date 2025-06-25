4e42d29de11cd4aaa3fec72fb59f423e64ae1913
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-06-25 16:29:03 +0200

    moved quota bar

--- src/gui/nmcgui/nmcaccountsettings.cpp
+++ src/gui/nmcgui/nmcaccountsettings.cpp
@@ -112,7 +112,7 @@
  
      quotaVLayout->addWidget(getUi()->quotaProgressBar);
      getUi()->quotaProgressBar->setStyleSheet(
-         "QProgressBar { background-color: #e5e5e5; color: black; border: 1px solid black; border-radius: 4px; } "
+         "QProgressBar { background-color: #e5e5e5; color: black; border: 1px solid black; border-radius: 4px; margin-left: 8px; } "
          "QProgressBar::chunk { background-color: #E20074; }");
      getUi()->quotaProgressBar->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
  
