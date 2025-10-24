ffeda1ba77f5f3d38f5a1466eff1a0250951e8be
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-10-24 11:52:03 +0200

    main settings dialog changes

--- src/gui/settingsdialog.h
+++ src/gui/settingsdialog.h
@@ -43,13 +43,18 @@ public:
 
     QWidget* currentPage();
 
+    QToolBar *getToolBar() const
+    {
+        return _toolBar;
+    }
+
 public slots:
     void showFirstPage();
     void showAccount(OCC::AccountState *account);
     void setInitialAccount(OCC::AccountState *account);
     void showIssuesList(OCC::AccountState *account);
     void slotSwitchPage(QAction *action);
-    void slotAccountAvatarChanged();
+    virtual void slotAccountAvatarChanged();
     void slotAccountDisplayNameChanged();
 
 signals:
@@ -67,12 +72,13 @@ private slots:
     void accountAdded(OCC::AccountState *);
     void accountRemoved(OCC::AccountState *);
 
-private:
+protected:
     void customizeStyle();
     void requestStyleUpdate();
     void updateAccountAvatar(const Account *account);
     void addSettingsPage(const QString &iconPath, const QString &title, QWidget *settingsPage, bool updateChannelAware = false);
 
+private:
     QAction *createColorAwareAction(const QString &iconName, const QString &fileName);
     QAction *createActionWithIcon(const QIcon &icon, const QString &text, const QString &iconPath = QString());
 
