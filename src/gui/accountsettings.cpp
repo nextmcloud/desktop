f30f8a17ac02ab7d470f58e870f3bd6c4dd87c5f
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-11-14 14:33:38 +0100

    show tabs

--- src/gui/accountsettings.cpp
+++ src/gui/accountsettings.cpp
@@ -192,8 +192,6 @@ AccountSettings::AccountSettings(AccountState *accountState, QWidget *parent)
 #endif
     new ToolTipUpdater(_ui->_folderList);
 
-const auto tabWidget = _ui->tabWidget;
-
 #if defined(BUILD_FILE_PROVIDER_MODULE)
     if (Mac::FileProvider::fileProviderAvailable()) {
         const auto fileProviderTab = _ui->fileProviderTab;
@@ -206,6 +204,7 @@ const auto tabWidget = _ui->tabWidget;
         fileProviderTab->setLayout(fpSettingsLayout);
     }
 #else
+    const auto tabWidget = _ui->tabWidget;
     const auto fileProviderTab = _ui->fileProviderTab;
     if (const auto fileProviderWidgetTabIndex = tabWidget->indexOf(fileProviderTab); fileProviderWidgetTabIndex >= 0) {
         tabWidget->removeTab(fileProviderWidgetTabIndex);
@@ -219,12 +218,6 @@ const auto tabWidget = _ui->tabWidget;
     connectionSettingsLayout->setContentsMargins(0, 0, 0, 0);
     connectionSettingsLayout->addWidget(networkSettings);
     connectionSettingsTab->setLayout(connectionSettingsLayout);
-
-    if (const auto connectionSettingsTabIndex = tabWidget->indexOf(connectionSettingsTab); connectionSettingsTabIndex >= 0) {
-        tabWidget->removeTab(connectionSettingsTabIndex);
-    }
-    tabWidget->setCurrentIndex(0);
-    tabWidget->tabBar()->hide();
 
     const auto mouseCursorChanger = new MouseCursorChanger(this);
     mouseCursorChanger->folderList = _ui->_folderList;
