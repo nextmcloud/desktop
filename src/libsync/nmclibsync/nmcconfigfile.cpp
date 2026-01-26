92e1274713cb588cb7c8239f940779de6db89443
memurats <Mauro-Efisio.Mura@t-systems.com>
2026-01-26 11:27:41 +0100

    fix connection error

--- src/libsync/nmclibsync/nmcconfigfile.cpp
+++ src/libsync/nmclibsync/nmcconfigfile.cpp
@@ -13,34 +13,35 @@
  */
 
 #include "nmcconfigfile.h"
+#include "theme.h"
 
 namespace OCC {
 
 bool NMCConfigFile::transferUsageData(const QString &connection) const
 {
-    QString con(connection);
-    if (connection.isEmpty())
-    {
-        con = defaultConnection();
-    }
-    QVariant fallback = getValue(m_transferUsageData, con, false);
+    const QString group =
+        connection.isEmpty()
+            ? Theme::instance()->appName()
+            : connection;
+
+    QVariant fallback = getValue(m_transferUsageData, group, false);
     fallback = getValue(m_transferUsageData, QString(), fallback);
 
-    QVariant value = getPolicySetting(m_transferUsageData, fallback);
+    const QVariant value = getPolicySetting(m_transferUsageData, fallback);
     return value.toBool();
 }
 
 void NMCConfigFile::setTransferUsageData(bool usageData, const QString &connection)
 {
-    QString con(connection);
-    if (connection.isEmpty())
-    {
-        con = defaultConnection();
-    }
-    QSettings settings(configFile(), QSettings::IniFormat);
-    settings.beginGroup(con);
+    const QString group =
+        connection.isEmpty()
+            ? Theme::instance()->appName()
+            : connection;
 
-    settings.setValue(m_transferUsageData, QVariant(usageData));
+    QSettings settings(configFile(), QSettings::IniFormat);
+    settings.beginGroup(group);
+
+    settings.setValue(m_transferUsageData, usageData);
     settings.sync();
 }
 
