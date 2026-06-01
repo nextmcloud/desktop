bb0f467d929a8620f67ee2da561f9a5844eab340
memurats <Mauro-Efisio.Mura@t-systems.com>
2026-06-01 17:31:24 +0200

    fix layout

--- src/gui/nmcgui/nmcaccountsettings.cpp
+++ src/gui/nmcgui/nmcaccountsettings.cpp
@@ -26,14 +26,26 @@
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QSizePolicy>
+#include <QIcon>
 
 namespace OCC {
+
+namespace {
+constexpr int panelPadding = 24;
+constexpr int contentWidth = 450;
+}
+
+#ifdef Q_OS_WIN
+    #define BACKGROUND_PALETTE "alternate-base"
+#else
+    #define BACKGROUND_PALETTE "light"
+#endif
 
 NMCAccountSettings::NMCAccountSettings(AccountState *accountState, QWidget *parent)
     : AccountSettings(accountState, parent)
     , m_liveAccountButton(new CustomButton(
         QCoreApplication::translate("", "ADD_LIVE_BACKUP"),
-        QIcon(QLatin1String(":/client/theme/NMCIcons/action-add.svg")),
+        QIcon(),
         this))
     , m_liveTitle(new QLabel(QCoreApplication::translate("", "LIVE_BACKUPS"), this))
     , m_liveDescription(new QLabel(QCoreApplication::translate("", "LIVE_DESCRIPTION"), this))
@@ -62,15 +74,17 @@ void NMCAccountSettings::setLayout()
     liveWidget->setAttribute(Qt::WA_StyledBackground, true);
     liveWidget->setStyleSheet(QStringLiteral(
         "#nmcLiveBackupPanel {"
-        " background: palette(base);"
-        " border-radius: 4px;"
+        " background: palette(" BACKGROUND_PALETTE ");"
+        " border-radius: 6px;"
         "}"
     ));
 
     auto *liveHLayout = new QHBoxLayout(liveWidget);
-    liveHLayout->setContentsMargins(8, 8, 8, 8);
+    liveHLayout->setContentsMargins(panelPadding, panelPadding, panelPadding, panelPadding);
+    liveHLayout->setSpacing(32);
 
     auto *liveVLayout = new QVBoxLayout();
+    liveVLayout->setSpacing(4);
 
     liveHLayout->addLayout(liveVLayout);
     liveHLayout->addStretch();
@@ -82,9 +96,9 @@ void NMCAccountSettings::setLayout()
 
     m_liveAccountButton->setStyleSheet(styleSheet.arg("0", "#E20074", "white", "#c00063", "13"));
     m_liveAccountButton->setFixedSize(180, 32);
-    m_liveAccountButton->setLeftIconMargin(4);
+    m_liveAccountButton->setIcon(QIcon());
 
-    liveHLayout->addWidget(m_liveAccountButton);
+    liveHLayout->addWidget(m_liveAccountButton, 0, Qt::AlignRight | Qt::AlignVCenter);
 
     liveVLayout->addWidget(m_liveTitle);
     m_liveTitle->setStyleSheet("font-size: 15px; font-weight: 600;");
@@ -93,7 +107,7 @@ void NMCAccountSettings::setLayout()
     m_liveDescription->setStyleSheet("font-size: 13px;");
     m_liveDescription->setText(QCoreApplication::translate("", "LIVE_BACKUPS_DESCRIPTION"));
     m_liveDescription->setWordWrap(true);
-    m_liveDescription->setFixedWidth(450);
+    m_liveDescription->setFixedWidth(contentWidth);
 
     getUi()->verticalLayout_2->addWidget(liveWidget);
 
@@ -102,47 +116,61 @@ void NMCAccountSettings::setLayout()
     quotaWidget->setAttribute(Qt::WA_StyledBackground, true);
     quotaWidget->setStyleSheet(QStringLiteral(
         "#nmcQuotaPanel {"
-        " background: palette(base);"
-        " border-radius: 4px;"
+        " background: palette(" BACKGROUND_PALETTE ");"
+        " border-radius: 6px;"
         "}"
     ));
+
     auto *magentaHLayout = new QHBoxLayout(quotaWidget);
-    magentaHLayout->setContentsMargins(8, 8, 8, 8);
+    magentaHLayout->setContentsMargins(panelPadding, panelPadding, panelPadding, panelPadding);
     magentaHLayout->setSpacing(32);
 
     auto *quotaVLayout = new QVBoxLayout();
     quotaVLayout->setSpacing(4);
-    quotaVLayout->addSpacing(12);
 
     quotaVLayout->addWidget(m_quotaInfoLabel);
-    m_quotaInfoLabel->setStyleSheet("QLabel { font-size: 18px; padding: 8px; font-weight: 500; }");
+    m_quotaInfoLabel->setFixedWidth(contentWidth);
+    m_quotaInfoLabel->setStyleSheet("QLabel { font-size: 18px; font-weight: 500; padding: 0px; }");
 
     quotaVLayout->addWidget(m_quotaProgressBar);
     m_quotaProgressBar->setRange(0, 100);
     m_quotaProgressBar->setTextVisible(false);
+    m_quotaProgressBar->setFixedWidth(contentWidth);
+    m_quotaProgressBar->setFixedHeight(8);
     m_quotaProgressBar->setStyleSheet(
-        "QProgressBar { background-color: #e5e5e5; color: black; border: 1px solid black; border-radius: 4px; margin-left: 8px; } "
-        "QProgressBar::chunk { background-color: #E20074; }");
-    m_quotaProgressBar->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        "QProgressBar {"
+        " background-color: #e5e5e5;"
+        " color: black;"
+        " border: 1px solid black;"
+        " border-radius: 4px;"
+        " margin: 0px;"
+        " padding: 0px;"
+        "} "
+        "QProgressBar::chunk {"
+        " background-color: #E20074;"
+        " border-radius: 3px;"
+        "}");
+    m_quotaProgressBar->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
 
     quotaVLayout->addWidget(m_quotaInfoText);
-    m_quotaInfoText->setStyleSheet("QLabel { font-size: 13px; padding: 8px; }");
+    m_quotaInfoText->setFixedWidth(contentWidth);
+    m_quotaInfoText->setStyleSheet("QLabel { font-size: 13px; padding: 0px; }");
 
     magentaHLayout->addLayout(quotaVLayout);
+    magentaHLayout->addStretch();
 
     auto *storageLinkButton = new QPushButton(QCoreApplication::translate("", "STORAGE_EXTENSION"), quotaWidget);
-    storageLinkButton->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+    storageLinkButton->setFixedSize(180, 32);
     storageLinkButton->setStyleSheet(
-        "QPushButton { height: 32px; width: 180px; border: 1px solid black; background-color: #ededed; "
+        "QPushButton { border: 1px solid black; background-color: #ededed; "
         "color: black; font-size: 13px; border-radius: 4px; } "
-        "QPushButton::hover { background-color: white; }");
+        "QPushButton:hover { background-color: white; }");
 
     connect(storageLinkButton, &QPushButton::clicked, this, []() {
         QDesktopServices::openUrl(QUrl(QStringLiteral("https://cloud.telekom-dienste.de/tarife")));
     });
 
-    magentaHLayout->addWidget(storageLinkButton);
-    magentaHLayout->addStretch();
+    magentaHLayout->addWidget(storageLinkButton, 0, Qt::AlignRight | Qt::AlignVCenter);
 
     getUi()->verticalLayout_2->addWidget(quotaWidget);
 
