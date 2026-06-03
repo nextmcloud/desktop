36672a3332d8c99959026862adb4413ae72432bb
memurats <Mauro-Efisio.Mura@t-systems.com>
2026-06-03 14:57:06 +0200

    fix limit spinbox

--- src/gui/nmcgui/nmcgeneralsettings.h
+++ src/gui/nmcgui/nmcgeneralsettings.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) by Eugen Fischer
+ * Copyright (C) by Mauro Mura
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
