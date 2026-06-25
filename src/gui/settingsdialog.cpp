1f39144307244d9f9872ff63daa4f8c095b53c72
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2026-06-25 12:36:40 +0200

    Change default height of settings dialog

--- src/gui/settingsdialog.cpp
+++ src/gui/settingsdialog.cpp
@@ -112,7 +112,7 @@ constexpr auto TOOLBAR_CSS = QLatin1String(
 
 const float buttonSizeRatio = 1.618f; // golden ratio
 constexpr auto settingsDialogDefaultWidth = 1024;
-constexpr auto settingsDialogDefaultHeight = 640;
+constexpr auto settingsDialogDefaultHeight = 666;
 const auto settingsNavigationIconTextSpacing = QLatin1String("  ");
 
 /** display name with two lines that is displayed in the settings
